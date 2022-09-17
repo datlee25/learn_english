34cf7a68856ec75ed3272d85f12453a247ab84bc
lethanhdat25 <lethanhdat025012@gmail.com>
2022-09-17 18:37:39 +0700

    fix: update get course

--- src/main/java/com/example/learning_english/payload/response/JwtResponse.java
+++ src/main/java/com/example/learning_english/payload/response/JwtResponse.java
@@ -17,15 +17,19 @@ public class JwtResponse {
     private String type = "Bearer";
     private String username;
     private String email;
+    private List<Integer> groupId;
+
     private String refreshToken;
+
     private List<String> roles;
 
-    public JwtResponse(int id, String accessToken, String refreshToken, String username, String email, List<String> roles) {
+    public JwtResponse(int id, String accessToken, String refreshToken, String username, String email,List<Integer> groupId, List<String> roles) {
         this.id = id;
         this.accessToken = accessToken;
         this.username = username;
         this.email = email;
         this.roles = roles;
+        this.groupId =groupId;
         this.refreshToken = refreshToken;
     }
 
