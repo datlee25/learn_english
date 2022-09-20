d1651a185b6af2f248361808bf626949dee44022
lethanhdat25 <lethanhdat025012@gmail.com>
2022-09-21 01:58:11 +0700

    fix: update Application

--- src/main/java/com/example/learning_english/LearningEnglishApplication.java
+++ src/main/java/com/example/learning_english/LearningEnglishApplication.java
@@ -44,7 +44,7 @@ public class LearningEnglishApplication {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://127.0.0.1:5500");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
