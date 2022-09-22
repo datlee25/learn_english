cb9625c73b6f61634ca9db59e8711dd1df6697d4
lethanhdat25 <lethanhdat025012@gmail.com>
2022-09-23 00:47:36 +0700

    fix: update code

--- src/main/java/com/example/learning_english/dto/Group/GroupDto.java
+++ src/main/java/com/example/learning_english/dto/Group/GroupDto.java
@@ -6,6 +6,7 @@ import lombok.*;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,4 +14,5 @@ import java.util.Set;
 public class GroupDto {
     private String name;
     private EGroupLevel groupLevel;
+    private BigDecimal price;
 }
